7464f35f002972bcaef0ef50169fa891f488c361
DonJNewman <Dnewman1934@yahoo.com>
2025-03-17 12:36:33 -0500

    Successful sprite rendering

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 import pygame
 from constants import *
+from player import Player
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0
+    player_one = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
     print('Starting Asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
@@ -11,8 +15,11 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill('#000000')
+        player_one.draw(screen)
         pygame.display.flip()
-        
+        dt = clock.tick(60) / 1000
+       
+
 
 if __name__ == "__main__":
     main()
