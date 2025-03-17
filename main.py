5692d7c289254699a99e9c9dd342177dc5ea8a14
DonJNewman <Dnewman1934@yahoo.com>
2025-03-17 13:38:31 -0500

    game loop revision - Groups

--- main.py
+++ main.py
@@ -6,18 +6,26 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     dt = 0
-    player_one = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+  
     print('Starting Asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()        
+    Player.containers = (updatable, drawable)
+    player_one = Player(SCREEN_WIDTH / 2,SCREEN_HEIGHT / 2)
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('#000000')
+        print(updatable)
+        updatable.update(dt)
+        for item in drawable:
+            item.draw(screen)
         
-        player_one.draw(screen)
-        player_one.update(dt)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
        
